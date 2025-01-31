cba03bba5303475bffedc58dad56a7d9375481d7
moshcat <mauricecathoverile@gmail.com>
2025-01-31 15:17:40 +0800

    progress: based style for reusable button
    making props for every button condition.

---
+++ src/components/reusable/ButtonComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <button
+    class="py-2 mt-8 text-white transition ease-in-out delay-200 rounded-md shadow-lg hover:shadow-lg hover:bg-slate-900 px-14 bg-weather-primary"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup></script>
