01e588d30668bb0cde864e6bcbf74b7eabb94d3a
moshcat <mauricecathoverile@gmail.com>
2025-02-03 10:10:11 +0800

    fix(search location): fix better endpoint for location results

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,18 @@
           placeholder="put the city name here"
           class="px-4 py-1 bg-transparent border-2 border-b rounded-md border-weather-tertiary w-96 focus:border-weather-secondary focus:outline-none"
         />
+        <ul
+          class="absolute w-96 rounded-lg px-1 py-2 shadow-md bg-white text-black top-[77px]"
+          v-if="geoSearchResult"
+        >
+          <li
+            v-for="searchResult in geoSearchResult"
+            :key="searchResult.id"
+            class="px-2 py-1 cursor-pointer"
+          >
+            {{ searchResult.properties.formatted }}
+          </li>
+        </ul>
       </div>
     </div>
   </main>
@@ -30,7 +42,7 @@ const getSearchResult = () => {
     if (searchQuery.value !== '') {
       try {
         const result = await axios.get(
-          `https://api.geoapify.com/v1/geocode/search?text=${searchQuery.value}&apiKey=${geoApi}`,
+          `https://api.geoapify.com/v1/geocode/search?text=${searchQuery.value}&apiKey=${geoApi}&type=state`,
         )
         geoSearchResult.value = result.data.features
         console.log(geoSearchResult.value)
