994dd7f551b7bd5c299fd7acce2f370a72b028ea
moshcat <mauricecathoverile@gmail.com>
2025-02-03 10:27:31 +0800

    feat: add a error handler if the search is empty

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,13 +13,19 @@
           class="absolute w-96 rounded-lg px-1 py-2 shadow-md bg-white text-black top-[77px]"
           v-if="geoSearchResult"
         >
-          <li
-            v-for="searchResult in geoSearchResult"
-            :key="searchResult.id"
-            class="px-2 py-1 cursor-pointer"
-          >
-            {{ searchResult.properties.formatted }}
-          </li>
+          <p v-if="searchError">Sorry, Something when wrong. Please try again</p>
+          <p v-if="!searchError && geoSearchResult.length === 0">
+            No results match your query, try a different term.
+          </p>
+          <template v-else>
+            <li
+              v-for="searchResult in geoSearchResult"
+              :key="searchResult.id"
+              class="px-2 py-1 cursor-pointer"
+            >
+              {{ searchResult.properties.formatted }}
+            </li>
+          </template>
         </ul>
       </div>
     </div>
@@ -34,6 +40,7 @@ const geoApi = import.meta.env.VITE_GEO_API
 const searchQuery = ref('')
 const queryTimeout = ref('')
 const geoSearchResult = ref('')
+const searchError = ref(null)
 
 // fungsi untuk mengambil data input lokasi
 const getSearchResult = () => {
@@ -42,11 +49,12 @@ const getSearchResult = () => {
     if (searchQuery.value !== '') {
       try {
         const result = await axios.get(
-          `https://api.geoapify.com/v1/geocode/search?text=${searchQuery.value}&apiKey=${geoApi}&type=state`,
+          `https://api.geoapify.com/v1/geocode/search?text=${searchQuery.value}&apiKey=${geoApi}`,
         )
         geoSearchResult.value = result.data.features
         console.log(geoSearchResult.value)
       } catch (error) {
+        searchError.value = true
         console.error(error)
       }
       return
